5a6cdd21a6c320906f53eb3753300bb755ec43a6
Théo Léquipé <lequipe.theo@gmail.com>
2020-11-09 16:45:07 +0100

    Modification script SQL

--- Script SQL/PACK_Insert.sql
+++
@@ -1,3 +0,0 @@
-insert into PACK (ID_PACK, NAME_PACK, PRICE) values (1, 'Fix San', 156);
-insert into PACK (ID_PACK, NAME_PACK, PRICE) values (2, 'Mat Lam Tam', 304);
-insert into PACK (ID_PACK, NAME_PACK, PRICE) values (3, 'Zoolab', 342);
